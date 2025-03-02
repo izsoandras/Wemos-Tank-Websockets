754302b4190ce1e2196808bce55d6bc4ea46c036
Miklos Daniel <daniel.miklos0@gmail.com>
2025-03-02 16:20:11 +0100

    replace emergency icon with a better one

--- gp-svelte/src/lib/constants.ts
+++ gp-svelte/src/lib/constants.ts
@@ -64,7 +64,7 @@ export const buttonMappings: GamepadButton[] = [
     icon: AudioLines,
   },
   {
-    label: "Emergency",
+    label: "Emergency", // aka Hazards
     gpio: 4,
     icon: TriangleAlert,
     favouredColor: colors.red,
